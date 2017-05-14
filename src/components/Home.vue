47dbb80bc22197167a2898acd7ce2c7336dbd816
thulioph <thulioph@gmail.com>
2017-05-14 15:22:49 -0300

    Adds the featues section component.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,11 +4,14 @@
       title="Home"
       subtitle="Aqui vem um subtitulo">
     </m-subheader>
+
+    <m-features></m-features>
   </div>
 </template>
 
 <script>
 import mSubheader from '@/components/Subheader';
+import mFeatures from '@/components/Features';
 
 export default {
   name: 'hello',
@@ -20,10 +23,13 @@ export default {
 
   components: {
     mSubheader,
+    mFeatures,
   },
-
 };
 </script>
 
 <style scoped>
+.container {
+  margin: 30px auto;
+}
 </style>
