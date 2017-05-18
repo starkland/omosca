9a5462ab2ca53760306db9c7fa894039045f84f4
Thulio Philipe <thulioph@gmail.com>
2017-05-18 15:59:11 -0300

    Adjusts the slider height.

--- src/components/Slide.vue
+++ src/components/Slide.vue
@@ -47,19 +47,19 @@ export default {
         {
           title: 'Title 1',
           description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi, praesentium!',
-          image: 'http://dummyimage.com/2024x640/4d494d/686a82.gif&text=placeholder+image',
+          image: 'http://dummyimage.com/2024x720/4d494d/686a82.gif&text=placeholder+image',
         },
 
         {
           title: 'Title 2',
           description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi, praesentium!',
-          image: 'http://dummyimage.com/2024x640/4d494d/686a82.gif&text=placeholder+image',
+          image: 'http://dummyimage.com/2024x720/4d494d/686a82.gif&text=placeholder+image',
         },
 
         {
           title: 'Title 3',
           description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi, praesentium!',
-          image: 'http://dummyimage.com/2024x640/4d494d/686a82.gif&text=placeholder+image',
+          image: 'http://dummyimage.com/2024x720/4d494d/686a82.gif&text=placeholder+image',
         },
       ],
     };
