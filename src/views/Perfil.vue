926cd1d02b6c3ba369d6ad579aecf0ca85c77f5e
thulioph <thulioph@gmail.com>
2017-05-31 21:07:11 -0300

    Adjusts the Profile page.

--- src/views/Perfil.vue
+++ src/views/Perfil.vue
@@ -1,12 +1,71 @@
 <template>
-  <h1>Perfil</h1>
+  <div>
+    <m-subheader
+      title="Perfil"
+      subtitle="Informações sobre o seu perfil">
+    </m-subheader>
+
+    <section class="section">
+      <div class="container">
+        <div class="columns">
+          <div class="column">
+            <div class="card">
+              <div class="card-content">
+                <div class="media">
+                  <div class="media-left">
+                    <figure class="image is-48x48">
+                      <img :src="userInfo.photoURL" :alt="userInfo.name">
+                    </figure>
+                  </div>
+
+                  <div class="media-content">
+                    <p class="title is-4">{{userInfo.name}}</p>
+                    <p class="subtitle is-6">
+                      <a :title="userInfo.email">{{userInfo.email}}</a>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
-// import LocalStorage from '../assets/js/LocalStorage';
+import mSubheader from '@/components/Subheader';
+import LocalStorage from '../assets/js/LocalStorage';
 
 export default {
   name: 'Perfil',
+
+  data() {
+    return {
+      userInfo: {},
+    };
+  },
+
+  components: {
+    mSubheader,
+  },
+
+  created() {
+    this.storage = new LocalStorage('user_info');
+
+    this.getUserInfo();
+  },
+
+  methods: {
+    getUserInfo() {
+      const userInfo = this.storage.get();
+
+      if (userInfo) {
+        this.userInfo = userInfo;
+      }
+    },
+  },
 };
 </script>
 
