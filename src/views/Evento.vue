33c3e3d5a893cc27ba71678abb62d557d7f66f15
Thulio Philipe <thulioph@gmail.com>
2017-06-26 13:15:27 -0300

    Adjust the event page.

--- src/views/Evento.vue
+++ src/views/Evento.vue
@@ -5,13 +5,36 @@
       subtitle="Aqui vem um subtitulo">
     </m-subheader>
 
-    <m-form></m-form>
+    <section class="section">
+      <div class="container">
+        <div class="heading">
+          <h1 class="title">
+            Cadastre seu evento
+          </h1>
+
+          <h2 class="subtitle">
+            Cadastreu seu evento novamente.
+          </h2>
+        </div>
+
+        <div class="columns">
+          <div class="column is-one-quarter">
+            <m-menu></m-menu>
+          </div>
+
+          <div class="column">
+            <m-form></m-form>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
 import mSubheader from '@/components/Subheader';
 import mForm from '@/components/Form';
+import mMenu from '@/components/Menu';
 
 export default {
   name: 'Evento',
@@ -19,6 +42,7 @@ export default {
   components: {
     mSubheader,
     mForm,
+    mMenu,
   },
 };
 </script>
@@ -26,5 +50,9 @@ export default {
 <style scoped>
 .container {
   margin: 30px auto;
+}
+
+.heading {
+  margin-bottom: 50px;
 }
 </style>
